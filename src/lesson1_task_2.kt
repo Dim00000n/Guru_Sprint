46e2a8d0d458fe6be6295a42c77321c56dfe1e70
Dim00000n <adammossad4@gmail.com>
2026-03-25 09:21:00 +0300

    GS-1-2

---
+++ src/lesson1_task_2.kt
@@ -0,0 +1,8 @@
+
+fun main(){
+    val applePrice : Double = 6.0
+    val applesAmount : Int = 5
+    var totalPrice = applePrice * applesAmount
+    totalPrice += totalPrice * 0.05
+    println("Результат  $totalPrice ")
+}
